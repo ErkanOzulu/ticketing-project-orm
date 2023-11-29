7b4a5646c2741b852aef4ef37f4ad57324920c93
Erkan <ErkanOzulu@users.noreply.github.com>
2023-11-29 14:34:34 +0300

    security

--- src/main/java/com/cydeo/TicketingProjectOrmApplication.java
+++ src/main/java/com/cydeo/TicketingProjectOrmApplication.java
@@ -4,20 +4,28 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketingProjectOrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketingProjectOrmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectOrmApplication.class, args);
+    }
 
 
+//we added extra class with mapper and  security dependencies to use them we need to add bean
 
+    @Bean
+    public ModelMapper mapper() {
 
-	@Bean
-	public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 
-		return new ModelMapper();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
