050cdb07a9e2c17a7aa814784acb6b5888f342d0
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-12 20:10:06 +0300

    Task Controller is done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -3,21 +3,20 @@ package com.cydeo.controller;
 import com.cydeo.dto.TaskDTO;
 import com.cydeo.entity.ResponseWrapper;
 import com.cydeo.enums.Status;
-import com.cydeo.service.ProjectService;
+
 import com.cydeo.service.TaskService;
-import com.cydeo.service.UserService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("/task")
+@RequestMapping("api/v1/task")
 public class TaskController {
 
     private final TaskService taskService;
