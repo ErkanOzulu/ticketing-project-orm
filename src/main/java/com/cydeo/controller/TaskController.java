e1faa18360c485f855cb110227abe4cf1d9ff03a
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-12 18:00:40 +0300

    Task controller is done

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,96 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.ResponseWrapper;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    private final TaskService taskService;
+
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+
+    }
+
+    @GetMapping()
+    public ResponseEntity<ResponseWrapper> getTasks() {
+
+        List<TaskDTO> list = taskService.listAllTasks();
+
+        return ResponseEntity.ok(new ResponseWrapper("All tasks are Successfully retrieved", list, HttpStatus.OK));
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<ResponseWrapper> getTaskById(@PathVariable("taskId") Long taskId) {
+
+        TaskDTO taskDTO = taskService.findById(taskId);
+
+        return ResponseEntity.ok(new ResponseWrapper("Task is successfully retrieved", taskDTO, HttpStatus.OK));
+
+    }
+
+    @PostMapping()
+    public ResponseEntity<ResponseWrapper> createTask(@RequestBody TaskDTO taskDto) {
+
+        taskService.save(taskDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Task is successfully created", HttpStatus.CREATED));
+    }
+
+
+    @DeleteMapping("{taskId}")
+    public ResponseEntity<ResponseWrapper> deleteTask(@PathVariable("taskId") Long taskId) {
+        taskService.delete(taskId);
+        return ResponseEntity.ok(new ResponseWrapper("Task is successfully deleted", HttpStatus.OK));
+    }
+
+    @PutMapping()
+    public ResponseEntity<ResponseWrapper> updateTask(@RequestBody TaskDTO taskDTO) {
+        taskService.update(taskDTO);
+
+        return ResponseEntity.ok(new ResponseWrapper("Task is successfully updated", taskDTO, HttpStatus.OK));
+
+    }
+
+    @GetMapping("/employee/pending-tasks")
+    public ResponseEntity<ResponseWrapper> employeePendingTasks() {
+        List<TaskDTO> list = taskService.listAllTasksByStatusIsNot(Status.COMPLETE);
+
+        return ResponseEntity.ok(new ResponseWrapper("Tasks are successfully retrieved", list, HttpStatus.OK));
+    }
+
+
+    @PutMapping("/employee/update/{id}")
+    public ResponseEntity<ResponseWrapper> employeeUpdateTask(@RequestBody TaskDTO taskDTO) {
+
+
+        taskService.updateStatus(taskDTO);
+
+        return ResponseEntity.ok(new ResponseWrapper("Tasks are successfully updated", taskDTO, HttpStatus.OK));
+
+    }
+
+    @GetMapping("/employee/archive")
+    public ResponseEntity<ResponseWrapper> employeeArchivedTasks() {
+        List<TaskDTO> taskDTOs = taskService.listAllTasksByStatus(Status.COMPLETE);
+
+        return ResponseEntity.ok(new ResponseWrapper("Tasks are successfully retrieved", taskDTOs, HttpStatus.OK));
+    }
+
+}
