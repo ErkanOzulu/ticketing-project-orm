0f6f2706573f74b712d01d6b698ef0c08702114e
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-13 16:30:47 +0300

    added KeycloakProperties, service and Impl for security

---
+++ src/main/java/com/cydeo/config/KeycloakProperties.java
@@ -0,0 +1,32 @@
+package com.cydeo.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+public class KeycloakProperties {
+
+    //  @Value()  to get info from application.properties
+    @Value("${keycloak.realm}")
+    private String realm;
+    @Value("${keycloak.auth-server-url}")
+    private String authServerUrl;
+    @Value("${keycloak.resource}")
+    private String clientId;
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+    @Value("${master.user}")
+    private String masterUser;
+    @Value("${master.user.password}")
+    private String masterUserPswd;
+    @Value("${master.realm}")
+    private String masterRealm;
+    @Value("${master.client}")
+    private String masterClient;
+
+
+}
