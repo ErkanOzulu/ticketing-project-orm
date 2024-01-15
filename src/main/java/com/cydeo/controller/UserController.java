92a52e568bfe6a2d91f3fffb2f951cae536727fd
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-15 09:12:45 +0300

    Applied customize log

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.annotation.DefaultExceptionMessage;
+import com.cydeo.annotation.ExecutionTimeLog;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.ResponseWrapper;
 import com.cydeo.exception.TicketingProjectException;
@@ -16,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/user")
-@Tag(name = "UserController",description = "User API")
+@Tag(name = "UserController", description = "User API")
 public class UserController {
 
     private final UserService userService;
@@ -25,6 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ExecutionTimeLog
     @GetMapping
     @RolesAllowed("Admin")
     @Operation(summary = "Get Users")
@@ -35,6 +37,7 @@ public class UserController {
         return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved", userDTOS, HttpStatus.OK));
     }
 
+    @ExecutionTimeLog
     @GetMapping("/{username}")
     @RolesAllowed("Admin")
     @Operation(summary = "Get User by username")
@@ -49,32 +52,34 @@ public class UserController {
     @PostMapping()
     @RolesAllowed("Admin")
     @Operation(summary = "Create User")
-    public ResponseEntity <ResponseWrapper> createUser(@RequestBody UserDTO user){
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user) {
 
         userService.save(user);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created",HttpStatus.CREATED));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created", HttpStatus.CREATED));
     }
+
     @PutMapping()
     @RolesAllowed("Admin")
     @Operation(summary = "Update User")
-    public ResponseEntity<ResponseWrapper>updateUser(@RequestBody UserDTO user){
+    public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user) {
 
         userService.update(user);
-        return ResponseEntity.ok(new ResponseWrapper("User is successfully updated", user,HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully updated", user, HttpStatus.OK));
 
     }
+
     @DeleteMapping("{username}")
     @RolesAllowed("Admin")
     @Operation(summary = "Delete User")
     @DefaultExceptionMessage(defaultMessage = "Failed to delete user")
-    public ResponseEntity<ResponseWrapper>deleteUser(@PathVariable("username") String username) throws TicketingProjectException {
+    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String username) throws TicketingProjectException {
 
 //        userService.deleteByUserName(username);
         userService.delete(username);
 
-        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted",HttpStatus.OK));
-      //  return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseWrapper("User is successfully deleted",HttpStatus.NO_CONTENT));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted", HttpStatus.OK));
+        //  return ResponseEntity.status(HttpStatus.NO_CONTENT).body(new ResponseWrapper("User is successfully deleted",HttpStatus.NO_CONTENT));
 
         //204 - HttpStatus.NO_CONTENT
 
