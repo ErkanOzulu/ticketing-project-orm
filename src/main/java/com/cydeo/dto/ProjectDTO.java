2d32f98dc482ce502a0b80e0245bd9f6087b4a0c
Erkan <ErkanOzulu@users.noreply.github.com>
2024-01-16 18:02:38 +0300

    added Builder

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -2,14 +2,14 @@ package com.cydeo.dto;
 
 import com.cydeo.enums.Status;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.time.LocalDate;
+import lombok.*;
 
+import java.time.LocalDate;
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ProjectDTO {
 
     private Long id;
--- src/main/java/com/cydeo/dto/RoleDTO.java
+++ src/main/java/com/cydeo/dto/RoleDTO.java
@@ -1,9 +1,10 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
